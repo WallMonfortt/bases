7d0437344510fc4e7f793c07b476551f07697d3b
Wal Monfortt <71857487+WallMonfortt@users.noreply.github.com>
2022-10-25 22:05:53 -0500

    if else

---
+++ sections/if-else.ts
@@ -0,0 +1,32 @@
+
+
+console.log('inicio del progrema!');
+
+export let isTired : boolean = !true;
+
+// if (isTired) {
+//     console.log('tomar cafe');
+// }else{
+//     console.log('tomar agua');
+// }
+
+(isTired)
+    ?console.log('tomar cafe')
+    :console.log('tomar agua');
+
+
+
+let grade: number = 40;
+
+if (grade >= 60) {
+    console.log('Aprobado');
+}else if (grade >= 50) {
+    console.log('Por poco');
+}else{ 
+    console.log('Reprobado');
+}
+
+
+
+
+console.log('fin del progrema!');
