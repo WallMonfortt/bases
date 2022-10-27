3c95952c4bd046508c66c99411ee3ea324cdc39b
Wal Monfortt <71857487+WallMonfortt@users.noreply.github.com>
2022-10-27 11:41:26 -0500

    switch

---
+++ sections/switch.ts
@@ -0,0 +1,17 @@
+
+
+export let weekday = 1;
+
+switch (weekday) {
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+    case 3:
+        console.log('Miercoles');
+        break;
+    default:
+        break;
+}
